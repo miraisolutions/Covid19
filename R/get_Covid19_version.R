e4ff9f75377508b71f8d7f6f6303bc928a554356
Gabriel <gabriel.foix@mirai-solutions.com>
2020-03-25 08:35:51 +0100

    Feature/38 improve header (#42)
    * Add News.md
    
    * Add Mirai logo inverted colours
    
    * Add get_Covid19 version()
    
    * Header a-la SmaRP with pure CSS styling (#38).
    
    Co-authored-by: fvitalini <francesca.vitalini@mirai-solutions.com>
    Co-authored-by: riccardoporreca <riccardo.porreca@mirai-solutions.com>

---
+++ R/get_Covid19_version.R
@@ -0,0 +1,12 @@
+#' @title Covid19 version
+#'
+#' @rdname get_Covid19_version
+#'
+#' @description Returns the version of the installed Covid19 package and app.
+#'
+#' @return The version of Covid19 as character string.
+#'
+#' @export
+get_Covid19_version <- function() {
+  as.character(utils::packageVersion("Covid19"))
+}
