13fbdab8ef44544616114f3f5626492859e99804
Guido Maggio <guido.maggio@mirai-solutions.com>
2020-05-31 13:38:31 +0200

    Updated with change of pop_data

--- man-roxygen/ex-mod_map.R
+++ man-roxygen/ex-mod_map.R
@@ -1,5 +1,7 @@
 if (interactive()) {
   library(shiny)
+  library(Covid19)
+  library(dplyr)
   long_title <- "Lorem ipsum dolor sit amet, consectetur adipisicing elit."
   ui <- fluidPage(
     tagList(
@@ -8,11 +10,22 @@ if (interactive()) {
     )
   )
   server <- function(input, output, session) {
-    orig_data <- reactive({
+    orig_data_aggregate <- reactive({
       orig_data <- get_timeseries_full_data() %>%
         get_timeseries_by_contagion_day_data()
+      orig_data_aggregate <- orig_data %>%
+        aggregate_province_timeseries_data() %>%
+        add_growth_death_rate() %>%
+        arrange(Country.Region) %>%
+        align_country_names_pop() %>%
+        get_pop_data() %>% # compute additional variables
+        align_country_names_pop_reverse() %>%
+        mutate(mortality_rate_1M_pop = round(10^6*deaths/population, digits = 3),
+               prevalence_rate_1M_pop = round(10^6*confirmed/population, digits = 3),
+               new_prevalence_rate_1M_pop = round(10^6*new_confirmed/population, digits = 3))
+      orig_data_aggregate
     })
-    callModule(Covid19:::mod_map_server, "map_ui", orig_data)
+    callModule(Covid19:::mod_map_server, "map_ui", orig_data_aggregate)
   }
   runApp(shinyApp(ui = ui, server = server), launch.browser = TRUE)
 }
