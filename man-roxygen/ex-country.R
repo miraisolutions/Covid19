f307b4168e68fcc3e9be94a4cfe56769e07bb229
Guido Maggio <guido.maggio@mirai-solutions.com>
2020-07-04 11:40:55 +0200

    Added

---
+++ man-roxygen/ex-country.R
@@ -0,0 +1,45 @@
+if (interactive()) {
+  library(shiny)
+  library(dplyr)
+  library(tidyr)
+  library(RColorBrewer)
+  library(COVID19)
+  library(plotly)
+  library(shinycssloaders)
+  library(DT)
+  library(grid)
+  library(scales)
+
+  sapply(file.path("R",list.files("R")), source)
+
+  long_title <- "Lorem ipsum dolor sit amet, consectetur adipisicing elit."
+  ui <- fluidPage(
+    Covid19Mirai:::mod_country_ui("country")
+  )
+  server <- function(input, output) {
+
+    orig_data <- get_datahub() %>%
+      get_timeseries_by_contagion_day_data()
+
+
+    pop_data = get_pop_datahub()
+    orig_data_aggregate = build_data_aggr(orig_data, pop_data)
+
+    n = 1000; w = 7
+
+    data_filtered <-
+      orig_data_aggregate %>%
+        Covid19Mirai:::rescale_df_contagion(n = n, w = w)
+
+    countries <- reactive({
+      data_filtered %>%
+        select(Country.Region) %>%
+        distinct()
+    })
+
+    callModule(mod_country_server, "country",
+               data_filtered = data_filtered, countries = countries)
+  }
+  runApp(shinyApp(ui = ui, server = server), launch.browser = TRUE)
+}
+
