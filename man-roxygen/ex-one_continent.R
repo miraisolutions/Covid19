8e5410abebbd2492b49a8d496b96df9ef6994424
Guido Maggio <guido.maggio@mirai-solutions.com>
2020-07-25 16:47:46 +0200

    roxygen

--- man-roxygen/ex-one_continent.R
+++ man-roxygen/ex-one_continent.R
@@ -11,11 +11,13 @@ if (interactive()) {
   library(shinycssloaders)
   library(RColorBrewer)
   require(DT)
+  require(COVID19)
+
   sapply(file.path("R",list.files("R")), source)
 
   long_title <- "Lorem ipsum dolor sit amet, consectetur adipisicing elit."
-  cont = "Northern America"
-  uicont = "northernamerica"
+  cont = "Europe"
+  uicont = "europe"
   ui <- fluidPage(
     tabPanel("Continents",
              tabsetPanel(
@@ -26,27 +28,30 @@ if (interactive()) {
   )
   server <- function(input, output) {
 
-    orig_data <- reactive({ get_datahub() %>%
+    orig_data <- #reactive({
+      get_datahub() %>%
         get_timeseries_by_contagion_day_data()
-    })
+    #})
 
     #pop_data = get_pop_data()
     pop_data = get_pop_datahub()
-    orig_data_aggregate = reactive({ build_data_aggr(orig_data(), pop_data)})
+    orig_data_aggregate = #reactive({
+      build_data_aggr(orig_data, pop_data)
+      #})
 
     countries_data_map <- Covid19Mirai:::load_countries_datahub_map(destpath = system.file("./countries_data", package = "Covid19Mirai"))
 
-    orig_data_aggregate_cont <- reactive({
-      orig_data_aggregate %>% filter(continent == cont)
-    })
+    # orig_data_aggregate_cont <- #reactive({
+    #   orig_data_aggregate %>% filter(continent == cont)
+    # #})
 
     n = 1000; w = 7
-    data_filtered <- reactive({
-      orig_data_aggregate_cont() %>%
-        rescale_df_contagion(n = n, w = w)
-    })
+    # data_filtered <- reactive({
+    #   orig_data_aggregate_cont() %>%
+    #     rescale_df_contagion(n = n, w = w)
+    # })
 
-    callModule(mod_continent_server, "cont_comparison", orig_data_aggregate = orig_data_aggregate_cont,
+    callModule(mod_continent_server, "cont_comparison", orig_data_aggregate = orig_data_aggregate,
                countries_data_map, n = n, w = w, pop_data, cont = cont, uicont = uicont)
   }
   runApp(shinyApp(ui = ui, server = server), launch.browser = TRUE)
