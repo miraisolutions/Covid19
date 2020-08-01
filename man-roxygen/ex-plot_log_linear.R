891801dedd41ec4a10e98e73d07bed54532d2058
Guido Maggio <guido.maggio@mirai-solutions.com>
2020-08-01 15:23:41 +0200

    Updated example

--- man-roxygen/ex-plot_log_linear.R
+++ man-roxygen/ex-plot_log_linear.R
@@ -7,7 +7,7 @@ if (interactive()) {
 
   long_title <- "Lorem ipsum dolor sit amet, consectetur adipisicing elit."
   ui <- fluidPage(
-    Covid19Mirai:::mod_plot_log_linear_ui("test")
+    Covid19Mirai:::mod_plot_log_linear_ui("test", select = FALSE, area = FALSE)
   )
   server <- function(input, output) {
 
@@ -53,7 +53,7 @@ if (interactive()) {
 
   long_title <- "Lorem ipsum dolor sit amet, consectetur adipisicing elit."
   ui <- fluidPage(
-    Covid19Mirai:::mod_plot_log_linear_ui("test")
+    Covid19Mirai:::mod_plot_log_linear_ui("test", select = FALSE, area = TRUE)
   )
   server <- function(input, output) {
 
@@ -88,4 +88,53 @@ if (interactive()) {
   }
   runApp(shinyApp(ui = ui, server = server), launch.browser = TRUE)
 }
+# with select
+if (interactive()) {
+  library(shiny)
+  library(dplyr)
+  library(ggplot2)
+  library(tidyr)
+  #sapply(file.path("R",list.files("R")), source)
+
+  long_title <- "Lorem ipsum dolor sit amet, consectetur adipisicing elit."
+  ui <- fluidPage(
+    Covid19Mirai:::mod_plot_log_linear_ui("test", select = TRUE, area = TRUE)
+  )
+  server <- function(input, output) {
+
+    orig_data <- get_datahub() %>%
+      get_timeseries_by_contagion_day_data()
+
+    n = 100
+    w = 7
+    data_filtered <-
+      orig_data %>%
+      Covid19Mirai:::rescale_df_contagion(n = n, w = w)
+
+
+    country_data <- data_filtered %>%
+        #filter(Country.Region %in% "Switzerland") %>%
+        filter(contagion_day > 0) %>%
+        arrange(desc(date))
+
+    levs <- sort_type_hardcoded()
+
+    df_area = purrr::map(unique(country_data$Country.Region),
+                           function(un) {
+                             dat = tsdata_areplot(country_data[country_data$Country.Region == un, ], levs, 10) #n = 0 for area plot
+                             dat$Country.Region = rep(un, nrow(dat))
+                             dat
+                           })
+    df_country = Reduce("rbind",df_area)
+
+    countries <- reactive({
+      country_data %>%
+        select(Country.Region) %>%
+        distinct() #%>% .$Country.Region
+    })
+    callModule(mod_plot_log_linear_server,"test", df = df_country, type = "area", countries = countries)
+  }
+  runApp(shinyApp(ui = ui, server = server), launch.browser = TRUE)
+}
+
 
