d1e090122599d7b33ed0cf42bd38623a05085612
Guido Maggio <guido.maggio@mirai-solutions.com>
2020-12-21 00:08:22 +0100

    Allowed just one plot

--- R/mod_growth_death_rate.R
+++ R/mod_growth_death_rate.R
@@ -1,38 +1,98 @@
-#' growth_death_rate UI Function
+
+#' barplot UI Function
 #'
 #' @description A shiny Module.
 #'
 #' @param id,input,output,session Internal parameters for {shiny}.
-#'
+#' @param plot1 character: ui_growth ui_death ui_stringency, choose UI element from uichoices for plot1 graph.
+#' @param plot2 character: ui_growth ui_death ui_stringency, choose UI element from uichoices for plot2 graph.
 #' @noRd
 #'
 #' @importFrom shiny NS tagList
 #' @importFrom shinycssloaders withSpinner
-mod_growth_death_rate_ui <- function(id){
+mod_barplot_ui <- function(id, plot1 = "ui_growth", plot2  = "ui_death"){
   ns <- NS(id)
+  uichoices = list(ui_growth = list(label = "",
+                                     choices = list("Over 3 days" = "growth_factor_3",
+                                                    # "Over 5 days" = "growth_factor_5",
+                                                    "Over one week" = "growth_factor_7",
+                                                    "Over 2 weeks" = "growth_factor_14"),
+                                     selected = "growth_factor_7"),
+                   ui_death =  list(label = "",
+                                     choices = varsNames(c("lethality_rate","deaths_rate_1M_pop")),
+                                     selected = "lethality_rate"),
 
-  tagList(
-    fluidRow(
-      column(6,
-             uiOutput(ns("title_growth_factor")),
-             selectInput(inputId = ns("growth_factor"), label = "",
-                          choices = list("Over 3 days" = "growth_factor_3",
-                                        # "Over 5 days" = "growth_factor_5",
-                                         "Over one week" = "growth_factor_7",
-                                          "Over 2 weeks" = "growth_factor_14")
-                         ,
-                          selected = "growth_factor_7"),
-             withSpinner(uiOutput(ns("plot_growth_factor")))
-      ),
-      column(6,
-             uiOutput(ns("title_death_toll")),
-             selectInput(inputId = ns("radio_pop"), label = "",
-                          choices = varsNames(c("lethality_rate","deaths_rate_1M_pop")),
-                          selected = "lethality_rate"),
-             withSpinner(uiOutput(ns("plot_death_rate")))
+                   ui_stringency =  list(label = "",
+                                          choices = varsNames(c("stringency_index")),
+                                          selected = "stringency_index"))
+
+  if (!is.null(plot1))
+      uichoice1 = uichoices[[plot1]]
+  else
+    uichoice1 = NULL
+  if (!is.null(plot2))
+    uichoice2 = uichoices[[plot2]]
+  else
+    uichoice2 = NULL
+
+  if (!is.null(uichoice1) && !is.null(uichoice2)) {
+    tagList(
+      div(id = id,
+
+      fluidRow(
+        column(6,
+               tagList(
+                 uiOutput(ns("title_plot_1")),
+                 selectInput(inputId = ns("plot_1"), label = uichoice1$label,
+                             choices = uichoice1$choices ,
+                             selected = uichoice1$selected),
+                 withSpinner(plotlyOutput(ns("plot_plot_1_hist"), height = 400)),
+                 div(htmlOutput(ns("caption1")), align = "center")
+               )
+        ),
+        column(6,
+               tagList(
+                 uiOutput(ns("title_plot_2")),
+                 selectInput(inputId = ns("plot_2"), label = uichoice2$label,
+                             choices = uichoice2$choices ,
+                             selected = uichoice2$selected),
+                 withSpinner(plotlyOutput(ns("plot_plot_2_hist"), height = 400)),
+                 div(htmlOutput(ns("caption2")), align = "center")
+               )
+        )
+      )
       )
     )
-  )
+  } else if (!is.null(uichoice1)){
+    # if (length(uichoice1$choices) == 1) {
+    #   tagList(
+    #     fluidRow(
+    #       uiOutput(ns("title_plot_1")),
+    #       textInput(inputId = ns("plot_1"), value =  uichoice1$selected, label = NULL),
+    #       withSpinner(plotlyOutput(ns("plot_plot_1_hist"), height = 400)),
+    #       div(htmlOutput(ns("caption1")), align = "center")
+    #     )
+    #   )
+    # } else {
+      tagList(
+        div(id = id,
+
+        fluidRow(
+          uiOutput(ns("title_plot_1")),
+          selectInput(inputId = ns("plot_1"), label = uichoice1$label,
+                      choices = uichoice1$choices ,
+                      selected = uichoice1$selected),
+          withSpinner(plotlyOutput(ns("plot_plot_1_hist"), height = 400)),
+          div(htmlOutput(ns("caption1")), align = "center")
+        )
+        )
+      )
+    #}
+
+  } else {
+    stop("If only one plot to be done then use plot1")
+  }
+
 }
 
 #' growth_death_rate Server Function
@@ -42,7 +102,9 @@ mod_growth_death_rate_ui <- function(id){
 #' @param w number of days of outbreak. Default 7
 #' @param n_highligth number of countries to highlight
 #' @param istop logical to choose title
-#' @param g_palette list of character vector of colors for the graph and legend of growth_factor and death_rate
+#' @param g_palette list of character vector of colors for the graph and legend of plot_1 and death_rate
+#' @param plottitle character vector giving title to plot1 and plot2
+#' @param pickvariable list of character vector of 1 variable name to use for sorting bars from lef, default is none
 #'
 #' @import dplyr
 #' @import tidyr
@@ -50,91 +112,102 @@ mod_growth_death_rate_ui <- function(id){
 #' @example ex-mod_growth_death_rate.R
 #'
 #' @noRd
-mod_growth_death_rate_server <- function(input, output, session, df, nn = 1000, w = 7,
+mod_barplot_server <- function(input, output, session, df, nn = 1000, w = 7,
                                          n_highligth = 5, istop = TRUE,
-                                         g_palette = list("growth_factor" = rate_colors["growth_factor"],
-                                                        "death_rate" = rate_colors["death_rate"])){
+                                         g_palette = list("plot_1" = barplots_colors[["growth_factor"]],
+                                                        "plot_2" = barplots_colors[["death_rate"]], calc = FALSE),
+                                         plottitle = c("Growth factor", "Death toll"),
+                                         pickvariable = list("plot_1" = character(0), "plot_2" = character(0))){
   ns <- session$ns
 
   # Help funcs ----
 
-  scale_mortality_rate <- function(dat){
-    df1 <- dat %>%
-      select_countries_n_cases_w_days(n = nn, w = 0) %>% # changed to w = 0 since we have only last day
-      filter( date == max(date))
-    df1
-  }
-
-  pick_rate <- function(df, rate){
-    df <-  df  %>%
-      bind_cols(df[, rate] %>% setNames("Value"))
-    df
-  }
-
-  pick_rate_hist <- function(df1, rate) {
-    df_plot <- df1 %>%
-      pick_rate(rate) %>%
-      arrange(desc(Value)) %>%
-      top_n(n_highligth, wt = Value) %>%
+  pick_rate_hist <- function(df1, rate, pickvar) {
+    if (length(pickvar) == 0)
+      pickvar = rate
+    df_plot <- df %>%
+      #arrange(desc(rate)) %>%
+      slice_max(!!sym(pickvar), n = n_highligth, with_ties = FALSE) %>%
       mutate(Country = factor(Country.Region, levels = .$Country.Region)) %>%
-      select(Country, Value)
+      select(Country, rate) %>% setNames(c("Country","Value"))
 
     df_plot
   }
 
+  isPlot2 = ifelse(length(plottitle) == 2, TRUE, FALSE)
   # Dataset ----
-  df_pop <- reactive(scale_mortality_rate(df))
+  #df_pop <- reactive(scale_mortality_rate(df))
 
-  df_base_plot1 <- reactive({pick_rate_hist( df_pop(), req(input$growth_factor))})
-  df_base_plot2 <- reactive({pick_rate_hist( df_pop(), req(input$radio_pop))})
+  df_base_plot1 <- reactive({pick_rate_hist(df, req(input$plot_1), pickvariable[["plot_1"]])})
+  if (isPlot2)
+    df_base_plot2 <- reactive({pick_rate_hist(df, req(input$plot_2), pickvariable[["plot_2"]])})
 
   # Plots ----
   # titles
   if (istop) {
-    output$title_growth_factor <- renderUI(div(h4(paste0("Current top ", n_highligth, " countries growth factor")), align = "center", style = "margin-top:20px; margin-bottom:20px;"))
-    output$title_death_toll <- renderUI(div(h4(paste0("Current top ", n_highligth, " countries death toll")), align = "center", style = "margin-top:20px; margin-bottom:20px;"))
+    varsort1 = if(length(pickvariable[["plot_1"]]) == 0) pickvariable[["plot_1"]] else  paste("by <", names(varsNames(pickvariable[["plot_1"]])), ">")
+    output$title_plot_1 <- renderUI(div(h4(HTML(paste0("Current top ", n_highligth, " countries ", varsort1, '<br/>',plottitle[1], '<br/>'))), align = "center", style = "margin-top:20px; margin-bottom:20px;"))
+    if (isPlot2) {
+      varsort2 = if(length(pickvariable[["plot_2"]]) == 0) pickvariable[["plot_2"]] else  paste("by<", names(varsNames(pickvariable[["plot_2"]])), ">")
+      output$title_plot_2 <- renderUI(div(h4(HTML(paste0("Current top ", n_highligth, " countries ", varsort2, '<br/>',plottitle[2], '<br/>'))), align = "center", style = "margin-top:20px; margin-bottom:20px;"))
+    }
+
   } else {
-    output$title_growth_factor <- renderUI(div(h4("Growth factor"), align = "center", style = "margin-top:20px; margin-bottom:20px;"))
-    output$title_death_toll <- renderUI(div(h4("Death toll"), align = "center", style = "margin-top:20px; margin-bottom:20px;"))
+    output$title_plot_1 <- renderUI(div(h4(plottitle[1]), align = "center", style = "margin-top:20px; margin-bottom:20px;"))
+    if (isPlot2)
+      output$title_plot_2 <- renderUI(div(h4(plottitle[2]), align = "center", style = "margin-top:20px; margin-bottom:20px;"))
   }
 
   # captions
-  #caption_growth_factor <- reactive({paste0("Computed as total confirmed cases today / total confirmed cases ", gsub("growth_factor_", "", req(input$growth_factor)) ," days ago.")})
-  #caption_growth_factor <- reactive({paste0("Total confirmed cases since ", gsub("growth_factor_", "", req(input$growth_factor)) ," days ago. / total confirmed cases in previous 30 days")})
-  caption_growth_factor <- reactive({ caption_growth_factor_fun(req(input$growth_factor))})
-  caption_death_rate_radio <- reactive({
-    if (req(input$radio_pop) == "lethality_rate") {
-      p <- "/ total confirmed cases today."
-    } else {
-      p <- "per 1 M population "
+
+  captionbarplot = function(var) {
+    if (grepl("^growth_factor",var)) {
+      caption_growth_factor_fun(var)
+    } else if (var %in% c("lethality_rate", "deaths_rate_1M_pop")) {
+      caption_death_fun(var)
+    }  else if (grepl("^stringency",var)) {
+      caption_stringency()
+    } else
+      names(varsNames(var))
+  }
+  caption_plot_1 <- reactive({
+    cap = captionbarplot(req(input$plot_1))
+    if (length(pickvariable[[1]])>0 && (pickvariable[[1]] != req(input$plot_1))) {
+      cap = c(cap, paste("Order from left to right by <", names(varsNames(pickvariable[[1]])), ">"))
     }
-    p
+    cap
   })
-  caption_death_rate <- reactive({paste0("Computed as total deaths today ",caption_death_rate_radio())})
-  #caption_countries <- paste0("More than ", n, " confirmed cases and outbreaks longer than ", w, " days.")
+  if (isPlot2) {
+    caption_plot_2 <- reactive({
+      cap = captionbarplot(req(input$plot_2))
+      if (length(pickvariable[[2]])>0 && (pickvariable[[2]] != req(input$plot_2))) {
+        cap = c(cap, paste("Order from left to right by <", names(varsNames(pickvariable[[2]])), ">"))
+      }
+      cap
+    })
+  }
 
   # plots
-  output$plot_growth_factor <- renderUI({
-    tagList(
-      plotlyOutput(ns("plot_growth_factor_hist"), height = 400),
-      div(p(caption_growth_factor()), align = "center")#,
-      #div(p(caption_countries), align = "center"),
-    )
-  })
 
-  output$plot_death_rate <- renderUI({
-    tagList(
-      plotlyOutput(ns("plot_death_rate_hist"), height = 400),
-      div(p(caption_death_rate()), align = "center")#,
-      #div(p(caption_countries), align = "center"),
-    )
-  })
+  output$caption1 <- renderText({
+    paste0("<p>", caption_plot_1(), sep = '</p>')
+      })
 
-  output$plot_growth_factor_hist <- renderPlotly({
-    g_palette_gf = g_palette[["growth_factor"]]
+  if (isPlot2) {
+      output$caption2 <- renderText({
+        paste0("<p>", caption_plot_2(), sep = '</p>')
+      })
+  }
+
+  is_percent_1 <- reactive({ifelse(req(input$plot_1) %in% .rate_vars, TRUE, FALSE)})
+
+  output$plot_plot_1_hist <- renderPlotly({
+    g_palette_1 = g_palette[["plot_1"]]
+    if (g_palette$calc)
+      g_palette_1 = palette_calc(g_palette_1, as.character(unique(df_base_plot1()$Country[order(df_base_plot1()$Value)])))
     p <- plot_rate_hist(df_base_plot1(), y_min = 1,
-                        #percent = is_percent(),
-                        g_palette =  g_palette_gf)
+                        percent = is_percent_1(),
+                        g_palette =  g_palette_1)
     p <- p %>%
       plotly::ggplotly() %>%
       plotly::layout(legend = list(orientation = "h", y = 1.1, yanchor = "bottom")
@@ -143,19 +216,22 @@ mod_growth_death_rate_server <- function(input, output, session, df, nn = 1000, 
     p
   })
 
-  is_percent <- reactive({ifelse(req(input$radio_pop) == "lethality_rate", TRUE, FALSE)})
-  output$plot_death_rate_hist <- renderPlotly({
-    message("is_percent = ", is_percent())
+  if (isPlot2) {
+    is_percent_2 <- reactive({ifelse(req(input$plot_2) %in% .rate_vars, TRUE, FALSE)})
+    output$plot_plot_2_hist <- renderPlotly({
 
-    g_palette_drf = g_palette[["death_rate"]]
+      g_palette_2 = g_palette[["plot_2"]]
 
-    p <- plot_rate_hist(df_base_plot2(), percent = is_percent(), g_palette =  g_palette_drf)
-    p <- p %>%
-      plotly::ggplotly() %>%
-      plotly::layout(legend = list(orientation = "h", y = 1.1, yanchor = "bottom")
-                     #xaxis = list(tickfont = list(size = 10))
-                     )
-    p
-  })
+      if (g_palette$calc)
+        g_palette_2 =  palette_calc(g_palette_2, as.character(unique(df_base_plot2()$Country[order(df_base_plot2()$Value)])))
+      p <- plot_rate_hist(df_base_plot2(), percent = is_percent_2(), g_palette =  g_palette_2)
+      p <- p %>%
+        plotly::ggplotly() %>%
+        plotly::layout(legend = list(orientation = "h", y = 1.1, yanchor = "bottom")
+                       #xaxis = list(tickfont = list(size = 10))
+        )
+      p
+    })
+  }
 
 }
