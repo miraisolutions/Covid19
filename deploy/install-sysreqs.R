2b986ad4905b1fa7c1efd5d6493be87b06608599
Riccardo Porreca <riccardo.porreca@mirai-solutions.com>
2025-10-07 12:00:50 +0200

    Refine `install-sysreqs.R`: jsonlite also available through pak's library
    * This also addresses the "trying to use CRAN without setting a mirror" problem running the script with `--vanilla`.

--- deploy/install-sysreqs.R
+++ deploy/install-sysreqs.R
@@ -1,6 +1,5 @@
-# We rely on pkgdepends from the library embedded in pak
+# We rely on jsonlite and pkgdepends from the library embedded in pak
 install.packages("pak", repos = "https://r-lib.github.io/p/pak/stable/")
-install.packages("jsonlite")
 .libPaths(c(system.file("library", package = "pak"), .libPaths()))
 pkgdepends::new_pkg_installation_proposal(
   names(jsonlite::read_json("renv.lock")$Packages), config = list(dependencies = FALSE)
