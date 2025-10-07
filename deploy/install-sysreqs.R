c5d718b3e9d505e6590e6acf090d535b39db7981
Riccardo Porreca <riccardo.porreca@mirai-solutions.com>
2025-10-07 10:58:26 +0200

    Refine `install-sysreqs.R` by using a temporary `.libPaths`

--- deploy/install-sysreqs.R
+++ deploy/install-sysreqs.R
@@ -1,6 +1,12 @@
+tmp_lib = tempfile()
+dir.create(tmp_lib)
+.libPaths(tmp_lib)
+on.exit(unlink(tmp_lib, recursive = TRUE))
+
 # We rely on jsonlite and pkgdepends from the library embedded in pak
 install.packages("pak", repos = "https://r-lib.github.io/p/pak/stable/")
-.libPaths(c(system.file("library", package = "pak"), .libPaths()))
+.libPaths(system.file("library", package = "pak"))
 pkgdepends::new_pkg_installation_proposal(
-  names(jsonlite::read_json("renv.lock")$Packages), config = list(dependencies = FALSE)
+  names(jsonlite::read_json("renv.lock")$Packages),
+  config = list(dependencies = FALSE)
 )$solve()$install_sysreqs()
